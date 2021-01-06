274a248a0c107281eb553e89b6583b84e5e5bf19
ersaumya <ersaumyarout@gmail.com>
2021-01-06 10:13:10 +0530

    routing refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,24 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () =>
+      import("./index/index.module").then((m) => m.IndexPageModule),
+  },
+  {
+    path: "",
+    loadChildren: () =>
+      import("./home/home.module").then((m) => m.HomePageModule),
+  },
   
-  {
-    path: '',
-    redirectTo: 'register',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { HomePage } from "./home.page";
+
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomePage,
+    children: [
+      {
+        path: "settings",
+        loadChildren: () =>
+          import("../pages/settings/settings.module").then(
+            (m) => m.SettingsPageModule
+          ),
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HomePageRoutingModule {}
